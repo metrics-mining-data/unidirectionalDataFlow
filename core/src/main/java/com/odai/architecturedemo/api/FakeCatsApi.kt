07dd94bf1151e590bdcd5f152d7201ab86a19ff8
Dorvaryn <ben@novoda.com>
2016-03-10 17:02:17 +0000

    Trigger modification only when it happens in the fake API

--- core/src/main/java/com/odai/architecturedemo/api/FakeCatsApi.kt
+++ core/src/main/java/com/odai/architecturedemo/api/FakeCatsApi.kt
@@ -13,18 +13,22 @@ class FakeCatsApi : CatApi {
         return Observable.just(favouriteCats).delay(3, TimeUnit.SECONDS)
     }
 
+    override fun getCats(): Observable<Cats> {
+        return Observable.just(Cats(listOf(Cat("Foo"), Cat("Bar")))).delay(3, TimeUnit.SECONDS)
+    }
+
     override fun addToFavourite(cat: Cat): Observable<Cat> {
-        favouriteCats = favouriteCats.add(cat)
         return Observable.just(cat).delay(3, TimeUnit.SECONDS)
+                .doOnNext {
+                    favouriteCats = favouriteCats.add(cat)
+                }
     }
 
     override fun removeFromFavourite(cat: Cat): Observable<Cat> {
-        favouriteCats = favouriteCats.remove(cat)
         return Observable.just(cat).delay(3, TimeUnit.SECONDS)
-    }
-
-    override fun getCats(): Observable<Cats> {
-        return Observable.just(Cats(listOf(Cat("Foo"), Cat("Bar")))).delay(3, TimeUnit.SECONDS)
+                .doOnNext {
+                    favouriteCats = favouriteCats.remove(cat)
+                }
     }
 
 }
